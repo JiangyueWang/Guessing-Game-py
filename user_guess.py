7cc42775d72eef5290309739b81df7d64a1198f2
Jiang <jiangyue0380@gmail.com>
2022-07-22 23:00:56 +1000

    updated comment in user_guess.py

--- user_guess.py
+++ user_guess.py
@@ -1,4 +1,5 @@
 def user_guess(shuffled_list):
+    # let user guess the index of the "o" in the list
     print(
         f'guess location of "o" in the list\nenter a number from {0} - {len(shuffled_list)-1}')
     user_guess = input('Enter here: ')
